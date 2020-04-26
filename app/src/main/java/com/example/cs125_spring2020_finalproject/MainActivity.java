1ba484347e6d71a30883bbea555ba87c87577ce1
TL <tianyu3@illinois.edu>
2020-04-26 15:44:23 -0500

    get request not working

--- app/src/main/java/com/example/cs125_spring2020_finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125_spring2020_finalproject/MainActivity.java
@@ -5,30 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String textSearch;
-
+    public static String textSearch;
+    private static final String fda = "https://api.nal.usda.gov/fdc/v1/search?";
+    private static final String apikey = "api_key=WfBOw7Htobl3oAqVVFbVdvEoDiN6IOpd6K6QeG8t";
+    public static List<String> searcheddata;
+    public static int hit = 0;
+    private RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button generate = findViewById(R.id.generateButton);
         Button clear = findViewById(R.id.clearButton);
-        final EditText search = findViewById(R.id.searchFDA);
+        Button searchbutton = findViewById(R.id.search);
+        queue = Volley.newRequestQueue(this);
+        EditText search = findViewById(R.id.searchFDA);
         textSearch = search.getText().toString();
-
+        //hitting searchbutton should pop a list of searched food.
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                search(textSearch);
+                display();
+            }
+        });
         //hitting generate button should generate a report.
         generate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -44,8 +66,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void search() {
-        fdaAPI.connect(textSearch, this);
+    public void search(String toSearch) {
+        final String input = "&query=" + toSearch;
+        JsonObjectRequest searchRequest = new JsonObjectRequest(Request.Method.GET, fda + apikey + input,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            hit = response.getInt("totalHits");
+                            /**
+                            JSONArray foods = response.getJSONArray("foods");
+                            for (int i = 0; i < foods.length(); i++) {
+                                JSONObject lista = foods.getJSONObject(i);
+                                String tem = lista.getString("description");
+                                searcheddata.add(tem);
+                            }*/
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(searchRequest);
+    }
+
+    public void display() {
+        Intent intent = new Intent(this, display.class);
+        startActivity(intent);
     }
 
     public void generateCreateDialog() {
---
+++ app/src/main/java/com/example/cs125_spring2020_finalproject/display.java
@@ -0,0 +1,25 @@
+package com.example.cs125_spring2020_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.cs125_spring2020_finalproject.MainActivity.hit;
+import static com.example.cs125_spring2020_finalproject.MainActivity.searcheddata;
+
+public class display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        TextView display = findViewById(R.id.textView3);
+        /**if (searcheddata == null) {
+            display.setText(MainActivity.textSearch);
+        } else {
+            display.setText(searcheddata.toString());
+        }*/
+        display.setText(hit);
+    }
+}
